bf05400008b53f251d350385b4e1b35ae9f73ffb
AshishNeupane <58897354+AshishNeupane@users.noreply.github.com>
2020-05-20 09:14:15 +0545

    la yo milyo hera bhai

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,5 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -26,10 +8,11 @@ import android.view.View;
 import android.widget.CalendarView;
 import android.widget.EditText;
 
-import java.util.Calendar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
     private mySQLiteDBHandler dbHandler;
     private EditText editText;
     private CalendarView calendarView;
@@ -40,34 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener(){
-        @Override
-            public void onClick(View v){
-            String message = "notifcations";
-            NotificationCompat.Builder builder= new NotificationCompat.Builder(
-                    MainActivity.this
-            )
-                    .setSmallIcon(R.drawable.ic_message)
-                    .setContentTitle("New notifications")
-                    .setContentText(message)
-                    .setAutoCancel(true);
-            Intent intent = new Intent(MainActivity.this,
-                    NotificationActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.putExtra("message",message);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
-                    0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(pendingIntent);
-            NotificationManager notificationManager = (NotificationManager)getSystemService(
-                    Context.NOTIFICATION_SERVICE
-            );
-             notificationManager.notify(0,builder.build());
-        }
-
-        });
 
         editText = findViewById(R.id.editText);
         calendarView = findViewById(R.id.calendarView2) ;
@@ -89,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e){
             e.printStackTrace();
         }
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
     }
     public void InsertDatabase(View view) {
         ContentValues contentValues = new ContentValues();
